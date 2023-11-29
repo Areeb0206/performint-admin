647900bcc8f145aa1cd456c96806e6eca6243559
AreebSafvi <areeb@fusiontecz.com>
2023-11-29 13:08:45 +0530

    first commit

--- src/redux/collection/service.js
+++
@@ -1,8 +0,0 @@
-import { DataService } from '../../config/dataService/dataService';
-
-const acceptCollectionComplaint = async (complaintId) => {
-  const response = await DataService.post(`/collection/complaints/${complaintId}/accept`);
-  return response;
-};
-
-export { acceptCollectionComplaint };
